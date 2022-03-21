66e9bbb60b90a7a6e92d31ab0dcbcb62032b550d
Antho <anthony.devweb@outlook.fr>
2022-03-21 14:27:25 +0100

    Cors include and back finish

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config({ path: "./config/.env" });
 require("./config/db");
 const { checkUser, requireAuth } = require("./middleware/auth.middleware");
+
 const app = express();
+
+const corsOption = {
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  'allowedHeaders': ["sessionId", "Content-Type"],
+  'exposedHeaders': ["sessionId"],
+  'methods': "GET,HEAD,PUT,PATCH,POST,DELETE",
+  'preflightContinue': false,
+};
+
+const cors = require("cors");
+
+app.use(cors({ corsOption }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
