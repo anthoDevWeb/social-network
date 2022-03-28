2bf3300c0fdde035bcf1b31a5c32e3e79a00424c
Antho <anthony.devweb@outlook.fr>
2022-03-28 10:09:20 +0200

    for make online

--- config/db.js
+++ config/db.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(
-    "mongodb+srv://" +
-      process.env.DB_USER_PASS +
-      "@cluster0.gasof.mongodb.net/mern-project"
-  )
+  .connect(process.env.DB)
   .then(() => console.log("connected to MongoDb"))
   .catch((err) => console.log("failed to connect to MongoDb ", err));
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path")
 const userRoutes = require("./routes/user.routes");
 const postRoutes = require("./routes/post.routes");
 const cookieParser = require("cookie-parser");
@@ -11,10 +12,10 @@ const app = express();
 const corsOption = {
   origin: process.env.CLIENT_URL,
   credentials: true,
-  'allowedHeaders': ["sessionId", "Content-Type"],
-  'exposedHeaders': ["sessionId"],
-  'methods': "GET,HEAD,PUT,PATCH,POST,DELETE",
-  'preflightContinue': false,
+  allowedHeaders: ["sessionId", "Content-Type"],
+  exposedHeaders: ["sessionId"],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
 };
 
 const cors = require("cors");
@@ -41,7 +42,13 @@ app.get("/jwtid", requireAuth, (req, res) => {
 app.use("/api/user", userRoutes);
 app.use("/api/post", postRoutes);
 
+app.use(express.static(path.join(__dirname, "/client")));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/public/', 'index.html'));
+});
+
 // server
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
